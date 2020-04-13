34080a56fd6851486ac82f38c7683465bce7b11e
tiefseeruebe <skservice@kahnts.de>
2020-04-13 21:29:23 +0200

    next step

--- main.py
+++ main.py
@@ -3,68 +3,71 @@
 # Author: Roman V. M.
 # Created on: 28.11.2014
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
-
+import ast
+import json
 import sys
+import urllib
+import urllib2
 from urllib import urlencode
 from urlparse import parse_qsl
+
 import xbmcgui
 import xbmcplugin
+import xbmc
+from StringIO import StringIO
+import gzip
+import ssl
+# from jsonpath_ng import jsonpath, parse
 
 # Get the plugin url in plugin:// notation.
+URL = 'url'
+REF_URL = 'ref_url'
 _url = sys.argv[0]
 # Get the plugin handle as an integer number.
 _handle = int(sys.argv[1])
 
-# Free sample videos are provided by www.vidsplay.com
-# Here we use a fixed set of properties simply for demonstrating purposes
-# In a "real life" plugin you will need to get info and links to video files/streams
-# from some web-site or online service.
-VIDEOS = {'Animals': [{'name': 'Crab',
-                       'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/04/crab-screenshot.jpg',
-                       'video': 'http://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4',
-                       'genre': 'Animals'},
-                      {'name': 'Alligator',
-                       'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/04/alligator-screenshot.jpg',
-                       'video': 'http://www.vidsplay.com/wp-content/uploads/2017/04/alligator.mp4',
-                       'genre': 'Animals'},
-                      {'name': 'Turtle',
-                       'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/04/turtle-screenshot.jpg',
-                       'video': 'http://www.vidsplay.com/wp-content/uploads/2017/04/turtle.mp4',
-                       'genre': 'Animals'}
-                      ],
-            'Cars': [{'name': 'Postal Truck',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/us_postal-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/us_postal.mp4',
-                      'genre': 'Cars'},
-                     {'name': 'Traffic',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/traffic1-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/traffic1.mp4',
-                      'genre': 'Cars'},
-                     {'name': 'Traffic Arrows',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/traffic_arrows-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/traffic_arrows.mp4',
-                      'genre': 'Cars'}
-                     ],
-            'Food': [{'name': 'Chicken',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/bbq_chicken-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/bbqchicken.mp4',
-                      'genre': 'Food'},
-                     {'name': 'Hamburger',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/hamburger-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/hamburger.mp4',
-                      'genre': 'Food'},
-                     {'name': 'Pizza',
-                      'thumb': 'http://www.vidsplay.com/wp-content/uploads/2017/05/pizza-screenshot.jpg',
-                      'video': 'http://www.vidsplay.com/wp-content/uploads/2017/05/pizza.mp4',
-                      'genre': 'Food'}
-                     ]}
+WIN_ID = 10000
+win = xbmcgui.Window(WIN_ID)
+WIDGET_PROP_NAME = 'libardjsonparser.widgets'
+CATEGORY = 'category'
+WIDGET_ID = 'widget_id'
+TITLE = 'title'
+LISTING = 'listing'
+TEASERS = 'teasers'
+
+VARS = {'name': 'home', 'client': 'ard', 'personalized': False}
+EXTS = {
+    'persistedQuery': {'version': 1, 'sha256Hash': '41ff7fbd45523453c78e2a780a83884ba5b66ce1483bdf1b3a3c3635491923a8'}}
+CATEGORIES = {'Overview': {
+    'url': 'https://api.ardmediathek.de/public-gateway?' +
+           'variables=' + urllib.quote(json.dumps(VARS)) +
+           '&extensions=' + urllib.quote(json.dumps(EXTS))
+}
+}
+
+
+def log(msg):
+    xbmc.log(str(msg))
+
+
+log("_handle")
+log(_handle)
+
+
+def is_integer(n):
+    try:
+        float(n)
+    except ValueError:
+        return False
+    else:
+        return float(n).is_integer()
 
 
 def get_url(**kwargs):
     """
     Create a URL for calling the plugin recursively from the given set of keyword arguments.
 
-    :param kwargs: "argument=value" pairs
+    :param kwargs: 'argument=value' pairs
     :type kwargs: dict
     :return: plugin call URL
     :rtype: str
@@ -86,61 +89,46 @@ def get_categories():
     :return: The list of video categories
     :rtype: types.GeneratorType
     """
-    return VIDEOS.iterkeys()
+    return CATEGORIES.iterkeys()
 
 
-def get_videos(category):
-    """
-    Get the list of videofiles/streams.
-
-    Here you can insert some parsing code that retrieves
-    the list of video streams in the given category from some site or server.
-
-    .. note:: Consider using `generators functions <https://wiki.python.org/moin/Generators>`_
-        instead of returning lists.
-
-    :param category: Category name
-    :type category: str
-    :return: the list of videos in the category
-    :rtype: list
-    """
-    return VIDEOS[category]
-
-
-def list_categories():
+def list_main_categories():
     """
     Create the list of video categories in the Kodi interface.
     """
     # Set plugin category. It is displayed in some skins as the name
     # of the current section.
-    xbmcplugin.setPluginCategory(_handle, 'My Video Collection')
+    xbmcplugin.setPluginCategory(_handle, 'ARD Mediathek')
     # Set plugin content. It allows Kodi to select appropriate views
     # for this type of content.
     xbmcplugin.setContent(_handle, 'videos')
     # Get video categories
-    categories = get_categories()
+    cat_iter = get_categories()
     # Iterate through categories
-    for category in categories:
+    for cat_key in cat_iter:
+        category = CATEGORIES[cat_key]
         # Create a list item with a text label and a thumbnail image.
-        list_item = xbmcgui.ListItem(label=category)
+        list_item = xbmcgui.ListItem(label=cat_key)
         # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
         # Here we use the same image for all items for simplicity's sake.
         # In a real-life plugin you need to set each image accordingly.
-        list_item.setArt({'thumb': VIDEOS[category][0]['thumb'],
-                          'icon': VIDEOS[category][0]['thumb'],
-                          'fanart': VIDEOS[category][0]['thumb']})
+        if 'thumb' in category:
+            list_item.setArt({'thumb': category['thumb'],
+                              'icon': category['thumb'],
+                              'fanart': category['thumb']})
         # Set additional info for the list item.
         # Here we use a category name for both properties for for simplicity's sake.
         # setInfo allows to set various information for an item.
         # For available properties see the following link:
         # https://codedocs.xyz/xbmc/xbmc/group__python__xbmcgui__listitem.html#ga0b71166869bda87ad744942888fb5f14
         # 'mediatype' is needed for a skin to display info for this ListItem correctly.
-        list_item.setInfo('video', {'title': category,
-                                    'genre': category,
+        list_item.setInfo('video', {TITLE: cat_key,
+                                    'genre': cat_key,
                                     'mediatype': 'video'})
+        list_item.setInfo(URL, category[URL])
         # Create a URL for a plugin recursive call.
         # Example: plugin://plugin.video.example/?action=listing&category=Animals
-        url = get_url(action='listing', category=category)
+        url = get_url(action=LISTING, category=cat_key)
         # is_folder = True means that this item opens a sub-list of lower level items.
         is_folder = True
         # Add our item to the Kodi virtual folder listing.
@@ -151,49 +139,185 @@ def list_categories():
     xbmcplugin.endOfDirectory(_handle)
 
 
-def list_videos(category):
+def url_get(url, use_proxy=False, verify_ssl=True):
+    # type: (str, bool, bool) -> str
+    log('get_widgets')
+    log('url')
+    log(url)
+
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:25.0) Gecko/20100101 Firefox/25.0')
+    req.add_header('Accept-Encoding', 'gzip, deflate')
+
+    if not verify_ssl:
+        log('set ssl context')
+        ctx = ssl.create_default_context()
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
+        log('set opener ...')
+        if use_proxy:
+            log('set proxy')
+            proxy = urllib2.ProxyHandler({'http': 'http://192.168.178.43:8080',
+                                          'https': 'http://192.168.178.43:8080'
+                                          })
+            opener = urllib2.build_opener(urllib2.HTTPSHandler(context=ctx), proxy)
+        else:
+            opener = urllib2.build_opener(urllib2.HTTPSHandler(context=ctx))
+        log('opener.open ...')
+        response = opener.open(req)
+    else:
+        response = urllib2.urlopen(req)
+    compressed = response.info().get('Content-Encoding') == 'gzip'
+    body = response.read()
+    response.close()
+    if compressed:
+        buf = StringIO(body)
+        f = gzip.GzipFile(fileobj=buf)
+        body = f.read()
+    return body
+
+
+def parse_teasers(widgets, widget_id):
+    log('parse_teasers')
+    log(widget_id)
+    log(widgets)
+
+    for widget in widgets:
+        if widget['id'] != widget_id:
+            continue
+        parse_teaser(widget)
+        break
+
+
+def parse_teaser(widget):
+    log('parse_teaser')
+    log(widget)
+    teasers = widget[TEASERS]
+    for teaser in teasers:  # type: list
+        item = {TITLE: teaser['shortTitle'],
+                URL: teaser['links']['target']['href'], '_type': 'dir'}
+        # log(teaser['shortTitle'].encode('ascii'))
+        add_dir_item(item)
+
+
+def list_category(params):
     """
     Create the list of playable videos in the Kodi interface.
 
-    :param category: Category name
-    :type category: str
+    :param params:
+    :type params: list
     """
+    log('list_category')
+    log(params)
     # Set plugin category. It is displayed in some skins as the name
     # of the current section.
-    xbmcplugin.setPluginCategory(_handle, category)
-    # Set plugin content. It allows Kodi to select appropriate views
-    # for this type of content.
-    xbmcplugin.setContent(_handle, 'videos')
-    # Get the list of videos in the category.
-    videos = get_videos(category)
-    # Iterate through videos.
-    for video in videos:
-        # Create a list item with a text label and a thumbnail image.
-        list_item = xbmcgui.ListItem(label=video['name'])
-        # Set additional info for the list item.
-        # 'mediatype' is needed for skin to display info for this ListItem correctly.
-        list_item.setInfo('video', {'title': video['name'],
-                                    'genre': video['genre'],
-                                    'mediatype': 'video'})
-        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
-        # Here we use the same image for all items for simplicity's sake.
-        # In a real-life plugin you need to set each image accordingly.
-        list_item.setArt({'thumb': video['thumb'], 'icon': video['thumb'], 'fanart': video['thumb']})
-        # Set 'IsPlayable' property to 'true'.
-        # This is mandatory for playable items!
-        list_item.setProperty('IsPlayable', 'true')
-        # Create a URL for a plugin recursive call.
-        # Example: plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4
-        url = get_url(action='play', video=video['video'])
-        # Add the list item to a virtual Kodi folder.
-        # is_folder = False means that this item won't open any sub-list.
-        is_folder = False
-        # Add our item to the Kodi virtual folder listing.
-        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+    if CATEGORY in params:
+        cat_key = params[CATEGORY]
+        xbmcplugin.setPluginCategory(_handle, cat_key)
+        url = CATEGORIES[cat_key]['url']
+    if WIDGET_ID in params:
+        widget_id = params[WIDGET_ID]
+        widgets_string = win.getProperty(WIDGET_PROP_NAME)
+        log("widget_string :")
+        log(widgets_string)
+        if widget_id > 0 and len(widgets_string) > 0:
+            widgets = ast.literal_eval(widgets_string)
+            parse_teasers(widgets, widget_id)
+    else:
+        if URL in params:
+            url = params[URL]
+
+        body_str = url_get(url)
+        body = json.loads(body_str)
+
+        if URL in params:
+            widgets = body['widgets']
+            for widget in widgets:
+                if widget['type'] == 'player_ondemand':
+                    list_item = xbmcgui.ListItem(label=widget[TITLE])
+                    list_item.setInfo('video', {'title': widget[TITLE],
+                                                'mediatype': 'video'})
+                    list_item.setProperty('IsPlayable', 'true')
+                    media_stream_list = widget['mediaCollection']['embedded']['_mediaArray'][0]['_mediaStreamArray']
+                    auto_idx = get_quality(media_stream_list)
+                    log('auto_idx')
+                    log(auto_idx)
+                    link = media_stream_list[auto_idx]['_stream']
+                    if link.startswith('//'):
+                        link = 'http:' + link
+                    log(link)
+                    url = get_url(action='play', video=link)
+                    is_folder = False
+                    xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+                if TEASERS in widget:
+                    parse_teaser(widget)
+        else:
+            body_str = url_get(url)
+            body = json.loads(body_str)
+            widgets = body['data']['defaultPage']['widgets']
+            win.setProperty(WIDGET_PROP_NAME, repr(widgets))
+
+            # Set plugin content. It allows Kodi to select appropriate views
+            # for this type of content.
+            xbmcplugin.setContent(_handle, 'videos')
+            # Iterate through widgets.
+            for widget in widgets:
+                item = {TITLE: widget[TITLE], WIDGET_ID: widget['id']}
+                add_dir_item(item)
     # Add a sort method for the virtual folder items (alphabetically, ignore articles)
-    xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    # xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
     # Finish creating a virtual folder.
     xbmcplugin.endOfDirectory(_handle)
+
+
+def get_quality(media_stream_list):
+    max_qual = -1
+    for i, media_stream in enumerate(media_stream_list):
+        if '_quality' in media_stream:
+            quality = media_stream['_quality']
+            if quality == 'auto':
+                return i
+            if is_integer(quality) and quality > max_qual:
+                max_qual = i
+    return max_qual
+
+
+def add_dir_item(item):
+    log('add_dir_item')
+    log(item)
+    list_item = xbmcgui.ListItem(label=item[TITLE])
+    # Set additional info for the list item.
+    # 'mediatype' is needed for skin to display info for this ListItem correctly.
+    list_item.setInfo('video', {TITLE: item[TITLE],
+                                'mediatype': 'video'})
+    # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
+    # Here we use the same image for all items for simplicity's sake.
+    # In a real-life plugin you need to set each image accordingly.
+    art = {}
+    if 'thumb' in item:
+        art['thumb'] = item['thumb']
+    if 'icon' in item:
+        art['icon'] = item['icon']
+    if 'fanart' in item:
+        art['fanart'] = item['fanart']
+    list_item.setArt(art)
+    # Set 'IsPlayable' property to 'true'.
+    # This is mandatory for playable items!
+    # list_item.setProperty('IsPlayable', 'true')
+    # Create a URL for a plugin recursive call.
+    # Example:
+    # plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4
+    if WIDGET_ID in item:
+        url = get_url(action=LISTING, widget_id=item[WIDGET_ID])
+    elif URL in item:
+        url = get_url(action=LISTING, url=item[URL])
+    else:
+        url = get_url(action=LISTING)
+    # Add the list item to a virtual Kodi folder.
+    # is_folder = False means that this item won't open any sub-list.
+    is_folder = True
+    # Add our item to the Kodi virtual folder listing.
+    xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
 
 
 def play_video(path):
@@ -209,22 +333,24 @@ def play_video(path):
     xbmcplugin.setResolvedUrl(_handle, True, listitem=play_item)
 
 
-def router(paramstring):
+def router(param_string):
     """
     Router function that calls other functions
     depending on the provided paramstring
 
-    :param paramstring: URL encoded plugin paramstring
-    :type paramstring: str
+    :param param_string: URL encoded plugin param_string
+    :type param_string: str
     """
-    # Parse a URL-encoded paramstring to the dictionary of
+    log('router')
+    log(param_string)
+    # Parse a URL-encoded param_string to the dictionary of
     # {<parameter>: <value>} elements
-    params = dict(parse_qsl(paramstring))
+    params = dict(parse_qsl(param_string))
     # Check the parameters passed to the plugin
     if params:
-        if params['action'] == 'listing':
+        if params['action'] == LISTING:
             # Display the list of videos in a provided category.
-            list_videos(params['category'])
+            list_category(params)
         elif params['action'] == 'play':
             # Play a video from a provided URL.
             play_video(params['video'])
@@ -232,11 +358,11 @@ def router(paramstring):
             # If the provided paramstring does not contain a supported action
             # we raise an exception. This helps to catch coding errors,
             # e.g. typos in action names.
-            raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
+            raise ValueError('Invalid param_string: {0}!'.format(param_string))
     else:
         # If the plugin is called from Kodi UI without any parameters,
         # display the list of video categories
-        list_categories()
+        list_main_categories()
 
 
 if __name__ == '__main__':
